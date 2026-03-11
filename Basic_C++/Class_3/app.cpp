8fa2efddf3a88b8c1e1c8f4d8870482017dcba6c
nimurr <nimurnerob404@gmail.com>
2026-03-11 16:00:23 +0600

    `Updated loop counters to start from 1 instead of 0`

--- Basic_C++/Class_3/app.cpp
+++ Basic_C++/Class_3/app.cpp
@@ -4,13 +4,12 @@ using namespace std;
 int main()
 {
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 1; i < 5; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 1; j < i; j++)
         {
-            cout << "* ";
+            cout << j;
         }
-        cout << endl;
     }
     return 0;
 }
